04b3b45309e13a0af5d1ce88992c399bc090aba8
Biniyam Girma <biniswengineer@gmail.com>
2025-12-24 13:05:01 +0300

    Created models for todo and user apps

--- Backend/backend/todo/models.py
+++ Backend/backend/todo/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+User = get_user_model()
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
